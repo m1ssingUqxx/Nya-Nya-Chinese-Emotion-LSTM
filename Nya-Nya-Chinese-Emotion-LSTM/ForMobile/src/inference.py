26784a13c225b788583c2c5a3645a8f9d6d03d5f
qweM1 <m1ssinguqxx@gmail.com>
2026-05-02 11:42:46 +0800

    Add files via upload

---
+++ Nya-Nya-Chinese-Emotion-LSTM/ForMobile/src/inference.py
@@ -0,0 +1,97 @@
+""" src/inference.py """
+import jieba
+import torch
+from config import STOP_SYNCS, DEVICE, EMOTION_LABELS, EMOTION_COLORS
+from src.dataset import SentimentDataSet
+from src.model import Nya_Nya_ChineseEmotionLSTM
+from src.utils import debug, save_records
+from src.colors import colors, gray
+
+from src.my_tokenizers import Tokenizer
+
+def self_test(text, model, tokenizer):
+    """ 对输入的文本进行测试 """
+    debug.function('文本测试中')
+    
+    # 分词处理
+    words = [x for x in jieba.cut(text, cut_all=False) if x not in STOP_SYNCS]
+    text_split = ' '.join(words)
+    
+    # 使用传入的Tokenizer进行编码
+    sequence = tokenizer.encode(text_split).ids
+    seq_len = len(sequence)
+
+    if seq_len <= 4:
+        # 优先处理 如果序列长度为0，则禁用停用词
+        if seq_len == 0:
+            debug.function('词量为0，禁用停用词')
+            text_split = ' '.join([x for x in text])
+
+        # 整个序列都为1，则对每个字进行单独分词
+        elif all(idx == 1 for idx in sequence):
+            debug.function('未识别到词组, 执行单字重分词')
+            text_split = ' '.join([x for x in text if x not in STOP_SYNCS])
+        # 整个序列中1占多数，则进行全模式分词
+        elif any(idx == 1 for idx in sequence):
+            debug.function('词量过小, 执行精确分词')
+            text_split = ' '.join([x for x in jieba.cut(str(text), cut_all=True) if x not in STOP_SYNCS])
+
+        sequence = tokenizer.encode(text_split).ids
+        seq_len = len(sequence)
+
+    # 转换为 Tensor 并移动到指定设备
+    inputs = torch.tensor([sequence], dtype=torch.long).to(DEVICE)
+
+    # 模型推理
+    with torch.no_grad():
+        model.eval()  # 切换到推理模式
+        outputs = model(inputs)  # 前向传播
+
+        # 计算概率分布
+        prob = torch.softmax(outputs, dim=1)
+
+        # 获取最大概率对应的索引
+        pred = torch.argmax(prob, dim=1)[0].item()
+
+        # 获取置信度
+        confidence = prob[0][pred]  
+        
+        # 反向数字标签字典为获取情感打印颜色
+        idx_to_label = {v: k for k, v in EMOTION_LABELS.items()}
+        sentiment = idx_to_label[pred]
+
+        debug.function('测试完成')
+
+        # 打印信息
+        print('-' * 40)
+        print(f'输入: \n{gray(f"{text}")}')
+        print(f'分词: \n{gray(f"{text_split}")}')
+        print(f'数字序列: \n{sequence}')
+        print(f'序列长度: {seq_len}')
+        print(f'情感分析: {EMOTION_COLORS[sentiment]}{sentiment:2}   {confidence * 100:.2f}%{colors["reset"]}')
+        print('其他情感分析：')
+        # 格式化打印
+        dis = []
+        for label, prob_value in zip(EMOTION_LABELS.keys(), prob[0]):
+            if label == sentiment:
+                continue
+            dis.append({'label': label, 'prob': prob_value.item()})
+        dis.sort(key=lambda x: x['prob'], reverse=True)
+
+        for item in dis:
+            print(f'  {EMOTION_COLORS[item["label"]]}{item["label"]:12} {item["prob"] * 100:5.2f}%{colors["reset"]}')
+        print('-' * 40)
+
+        # 加入记录中便于输出信息与记录
+        records = [{
+            'input': text,
+            'text_split': text_split,
+            'sequence': sequence,
+            'sequence_len': seq_len,
+            'result': dis,
+        }]
+
+        # 输出结束后，在列表最开始插入概率最大的标签用于保存记录
+        dis.insert(0, {'label': sentiment, 'prob': confidence})
+
+        save_records(records, file_name='test_records.txt', mode='test')  # 保存
---
+++ Nya-Nya-Chinese-Emotion-LSTM/ForPC/src/model.py
@@ -0,0 +1,102 @@
+""" src/model.py """
+import re
+import torch
+import torch.nn as nn
+import json
+
+from pathlib import Path
+from config import DEVICE
+from src.utils import debug, save_model_name
+from src.attention import Attention
+
+class Nya_Nya_ChineseEmotionLSTM(nn.Module):
+    """ LSTM模型类 """
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers=2):
+        debug.model('正在初始化LSTM模型')
+        super().__init__()
+        # 词嵌入层
+        self.embedding = nn.Embedding (
+            num_embeddings=vocab_size, # 要嵌入的单词总数
+            embedding_dim=embedding_dim, # 词向量维度
+            padding_idx=0 # 数字 0 作为填充符
+        )
+        # RNN LSTM层
+        self.lstm = nn.LSTM (
+            input_size=embedding_dim, # 输入大小（维度）
+            hidden_size=hidden_dim, # 隐层大小（维度）
+            num_layers=num_layers, # LSTM层数
+            batch_first=True, # 输入形状为 (batch, seq_len, feature) 否则为(seq_len, batch, feature)
+            bidirectional=True, # 开启双向LSTM
+            dropout=0.5 if num_layers > 1 else 0 # 正则化率
+        )
+        # 线性分类层 双层LSTM会导致两倍的输出维度
+        self.linear = nn.Linear(hidden_dim * 2, 7) # 七分类
+        # 注意力层
+        self.attention = Attention(hidden_dim * 2)
+
+        # 正则化
+        self.dropout = nn.Dropout(0.5)
+        
+    def forward(self, x):
+        """前向传播"""
+        x = self.embedding(x)
+        x = self.dropout(x)
+        x, (h, c) = self.lstm(x)
+        x, w = self.attention(x)
+        x = self.linear(x)
+        return x
+
+def load_model(model_path, sava_path=True, vocab_size=None, embedding_dim=None, hidden_dim=None, num_layers=None, find_best=False):
+    """ 加载模型 """
+    if find_best: # 如果需要寻找最佳模型
+        max_accuracy = 0
+        best_model_path = None
+        
+        # 寻找最佳准确率的模型
+        for model_file in Path(model_path).glob('*.pth'):
+            try:
+                checkpoint = torch.load(model_file, map_location=DEVICE, weights_only=True)
+                accuracy = checkpoint.get("accuracy", 0)
+            except: # 损坏的模型直接跳过
+                continue
+            # 对比找到准确率最大的模型
+            if accuracy > max_accuracy:
+                max_accuracy = accuracy
+                best_model_path = model_file
+        
+        if best_model_path is None:
+            raise ValueError(f"在 {model_path} 中没有找到有效的模型文件")
+
+        checkpoint = torch.load(best_model_path, map_location=DEVICE)
+    else: # 如果不需要寻找最佳模型
+        checkpoint = torch.load(model_path, map_location=DEVICE)
+    
+    model = Nya_Nya_ChineseEmotionLSTM(
+        vocab_size=checkpoint['vocab_size'],
+        embedding_dim=checkpoint['embedding_dim'],
+        hidden_dim=checkpoint['hidden_dim'],
+        num_layers=checkpoint['num_layers']
+    )
+    model.to(DEVICE)
+    
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    if find_best:
+        # 保存模型名
+        if sava_path:
+            save_model_name('session_state.json', best_model_path.name)
+
+        print(f"已自动加载最佳模型: {best_model_path.name}")
+        print(f"模型准确率: {max_accuracy * 100:.2f}%")
+    else:
+        if sava_path:
+            save_model_name('session_state.json', model_path.name)
+
+        print(f"已加载模型: {model_path.name}")
+        print(f"模型准确率: {checkpoint['accuracy'] * 100:.2f}%")
+    
+    print(f"模型参数: vocab={checkpoint['vocab_size']}, embedding={checkpoint['embedding_dim']}, "
+          f"hidden={checkpoint['hidden_dim']}, layers={checkpoint['num_layers']}")
+    print('-' * 40)
+    
+    return model
